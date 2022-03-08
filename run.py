4d0c973694392ee2f9ce0338177305e34b895999
Pete Machin <designer@deadpixelsdesign.com>
2022-03-08 23:33:40 +0000

    Add import random for random selection of cpu choices then add a while loop for the player giving them the 4 options. Run print statement to test each

--- run.py
+++ run.py
@@ -5,4 +5,8 @@ choices = ['rock','paper','scissors','gun']
 cpu = random.choice(choices)
 player = None
 
-print(choices)
+while player not in choices:
+    player = input('rock, paper, scissors, or gun?: ')
+
+print(player)
+

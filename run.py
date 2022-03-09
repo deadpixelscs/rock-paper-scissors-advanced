76bc2ada618c5fa5870137ab8b27fd945a77953b
Pete Machin <designer@deadpixelsdesign.com>
2022-03-09 00:09:34 +0000

    Add else if statements for the player to determine the possible outcomes if the player selects rock.

--- run.py
+++ run.py
@@ -9,7 +9,21 @@ while player not in choices:
     player = input('rock, paper, scissors, or gun?: ')
 
 if player == cpu:
+    print('cpu: ',cpu)
     print('player: ',player)
-    
-print('cpu: ',cpu)
+    print('Unlucky!, that games was a Tie!')
 
+elif player == 'rock':
+    if cpu == 'paper':
+        print('cpu: ',cpu)
+        print('player: ',player)
+        print('Hahahaha...You Lose!')
+    if cpu == 'scissors':
+        print('cpu: ',cpu)
+        print('player: ',player)
+        print('Welldone you beat me!')
+    if cpu == 'gun':
+        print('cpu: ',cpu)
+        print('player: ',player)
+        print('Welldone you won!')
+        
